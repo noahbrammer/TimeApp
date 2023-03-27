9cd6bcf8ca32ddd90a43b29e0b8784fc33bab82c
Noah Brammer <66965866+noahbrammer@users.noreply.github.com>
2023-03-26 19:33:38 -0500

    Update power-up.js

--- power-up.js
+++ power-up.js
@@ -1,22 +1,38 @@
 const t = TrelloPowerUp.iframe();
 
-const startTimer = function (t) {
-  // ...
+const startTimer = async function (t) {
+  const startTime = Date.now();
+  await t.set('card', 'shared', 'startTime', startTime);
+  await t.set('card', 'shared', 'timerRunning', true);
+  t.render();
 };
 
-const stopTimer = function (t) {
-  // ...
+const stopTimer = async function (t) {
+  const startTime = await t.get('card', 'shared', 'startTime');
+  const totalTime = await t.get('card', 'shared', 'totalTime', 0);
+  const elapsedTime = Date.now() - startTime;
+  const newTotalTime = totalTime + elapsedTime;
+
+  await t.set('card', 'shared', 'totalTime', newTotalTime);
+  await t.set('card', 'shared', 'timerRunning', false);
+  t.render();
 };
 
-t.render(async function () {
-  const context = await t.getContext();
-  const pluginData = await t.getAll();
-
-  if (pluginData.board.shared.timerRunning) {
-    t.set('card', 'button', 'Stop Timer', stopTimer, true);
-  } else {
-    t.set('card', 'button', 'Start Timer', startTimer, true);
+TrelloPowerUp.initialize({
+  'card-buttons': async function (t, options) {
+    const timerRunning = await t.get('card', 'shared', 'timerRunning', false);
+    if (timerRunning) {
+      return [{
+        icon: './icon.png',
+        text: 'Stop Timer',
+        callback: stopTimer
+      }];
+    } else {
+      return [{
+        icon: './icon.png',
+        text: 'Start Timer',
+        callback: startTimer
+      }];
+    }
   }
-
-  return t.sizeTo('#content').done();
 });
