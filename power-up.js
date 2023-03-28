aac00f904ed6329f78b1500f39166f48e72210dd
Noah Brammer <66965866+noahbrammer@users.noreply.github.com>
2023-03-27 20:37:27 -0500

    Update power-up.js

--- power-up.js
+++ power-up.js
@@ -1,13 +1,13 @@
 const t = TrelloPowerUp.iframe();
 
-const startTimer = async function (t) {
+window.startTimer = async function (t) {
   const startTime = Date.now();
   await t.set('card', 'shared', 'startTime', startTime);
   await t.set('card', 'shared', 'timerRunning', true);
   t.render();
 };
 
-const stopTimer = async function (t) {
+window.stopTimer = async function (t) {
   const startTime = await t.get('card', 'shared', 'startTime');
   const totalTime = await t.get('card', 'shared', 'totalTime', 0);
   const elapsedTime = Date.now() - startTime;
@@ -17,6 +17,3 @@ const stopTimer = async function (t) {
   await t.set('card', 'shared', 'timerRunning', false);
   t.render();
 };
-
-window.startTimer = startTimer;
-window.stopTimer = stopTimer;
